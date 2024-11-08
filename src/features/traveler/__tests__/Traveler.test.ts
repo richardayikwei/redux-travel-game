c2531ba903a9047711835de0c6bc5df7f7548653
Richard Ayikwei <richardayikweibee@gmail.com>
2024-11-08 11:27:11 +0000

    test for the buy reducer

--- src/features/traveler/__tests__/Traveler.test.ts
+++ src/features/traveler/__tests__/Traveler.test.ts
@@ -29,4 +29,24 @@ describe(' buy traveler reducer', () => {
     test('should return intialState without any changes if payload is a zero', () => {
         expect(travelerReducer(initialState, buy(0))).toBe(initialState);
     })
+    test('should return intialState without any changes if reputation is a zero', () => {
+        expect(travelerReducer(initialState, buy(0))).toBe(initialState);
+    })
+    test('should return intialState without any changes if reputation is is a negative number', () => {
+        expect(travelerReducer(initialState, buy(-7))).toBe(initialState);
+    })
+    test('should return intialState without any changes if payload is greater than cash', () => {
+        expect(travelerReducer(initialState, buy(101))).toBe(initialState);
+    })
+    test('should update supplies and cash if reputation is not less than or equal to zero and cash is not less than or equal to zero', () => {
+        const result = {
+            cash: 99,
+            days: 0,
+            supplies: 2,
+            fatigue: 0,
+            disrepair: 0,
+            reputation: 100,
+        };
+        expect(travelerReducer(initialState, buy(1))).toStrictEqual(result);
+    })
 });
