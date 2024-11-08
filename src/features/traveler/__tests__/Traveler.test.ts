66cb18bc82f7984ca23514840b17eda4232b7bb5
Richard Ayikwei <richardayikweibee@gmail.com>
2024-11-08 12:29:34 +0000

    gather tests

--- src/features/traveler/__tests__/Traveler.test.ts
+++ src/features/traveler/__tests__/Traveler.test.ts
@@ -10,34 +10,25 @@ import travelerReducer, {
     reset,
 } from "../travelerSlice";
 import '@testing-library/jest-dom';
-
+import { initialState,zeroReputationState,negativeReputationState, fatiguedState,  } from "./testStates";
 
 
 describe(' buy traveler reducer', () => {
-    const initialState = {
-        cash: 100,
-        days: 0,
-        supplies: 0,
-        fatigue: 0,
-        disrepair: 0,
-        reputation: 100,
-    }
-
     test('should return intialState without any changes if payload is a negative number', () => {
         expect(travelerReducer(initialState, buy(-1))).toBe(initialState);
-    })
+    });
     test('should return intialState without any changes if payload is a zero', () => {
         expect(travelerReducer(initialState, buy(0))).toBe(initialState);
-    })
+    });
     test('should return intialState without any changes if reputation is a zero', () => {
-        expect(travelerReducer(initialState, buy(0))).toBe(initialState);
-    })
-    test('should return intialState without any changes if reputation is is a negative number', () => {
-        expect(travelerReducer(initialState, buy(-7))).toBe(initialState);
-    })
+        expect(travelerReducer(zeroReputationState, buy(1))).toBe(zeroReputationState);
+    });
+    test('should return intialState without any changes if reputation is a negative number', () => {
+        expect(travelerReducer(negativeReputationState, buy(1))).toBe(negativeReputationState);
+    });
     test('should return intialState without any changes if payload is greater than cash', () => {
         expect(travelerReducer(initialState, buy(101))).toBe(initialState);
-    })
+    });
     test('should update supplies and cash if reputation is not less than or equal to zero and cash is not less than or equal to zero', () => {
         const result = {
             cash: 99,
@@ -48,5 +39,34 @@ describe(' buy traveler reducer', () => {
             reputation: 100,
         };
         expect(travelerReducer(initialState, buy(1))).toStrictEqual(result);
+    })
+});
+
+describe('gather travel reducer', () => {
+    test('should return intialState without any changes if payload is a negative number', () => {
+        expect(travelerReducer(initialState, gather(-1))).toBe(initialState);
+    })
+    test('should return intialState without any changes if payload is a zero', () => {
+        expect(travelerReducer(initialState, gather(0))).toBe(initialState);
+    })
+    test('should return zeroReputationState without any changes if reputation is a zero', () => {
+        expect(travelerReducer(zeroReputationState, gather(1))).toBe(zeroReputationState);
+    })
+    test('should return negativeReputationState without any changes if reputation is a negative number', () => {
+        expect(travelerReducer(negativeReputationState, gather(1))).toBe(negativeReputationState);
+    })
+    test('should return fatiguedState without any changes if energy required to gather exceeds maximum fatigue level of 100', () => {
+        expect(travelerReducer(fatiguedState, gather(1))).toBe(fatiguedState);
+    })
+    test('should update supplies, fatigue and days if reputation is not less than or equal to zero and fatigue levels do not exceed 100 after gathering', () => {
+        const result = {
+            cash: 100,
+            days: 1,
+            supplies: 3,
+            fatigue: 2,
+            disrepair: 0,
+            reputation: 100,
+        }
+        expect(travelerReducer(initialState, gather(1))).toStrictEqual(result);
     })
 });
