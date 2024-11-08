e8a86f233a4b7898100df0c444498a25a285f62e
Richard Ayikwei <richardayikweibee@gmail.com>
2024-11-08 12:29:34 +0000

    gather tests

---
+++ src/features/traveler/__tests__/testStates.ts
@@ -0,0 +1,33 @@
+export const initialState = {
+    cash: 100,
+    days: 0,
+    supplies: 0,
+    fatigue: 0,
+    disrepair: 0,
+    reputation: 100,
+}
+
+export const zeroReputationState = {
+    cash: 100,
+    days: 0,
+    supplies: 0,
+    fatigue: 0,
+    disrepair: 0,
+    reputation: 0,
+}
+export const negativeReputationState = {
+    cash: 100,
+    days: 0,
+    supplies: 0,
+    fatigue: 0,
+    disrepair: 0,
+    reputation: -1,
+}
+export const fatiguedState = {
+    cash: 100,
+    days: 0,
+    supplies: 0,
+    fatigue: 99,
+    disrepair: 0,
+    reputation: 100,
+}
