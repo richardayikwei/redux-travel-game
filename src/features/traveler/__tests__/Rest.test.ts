20019c576d1f264f6578d0f6b4ce648e661a9028
Richard Ayikwei <richardayikweibee@gmail.com>
2024-11-08 18:00:34 +0000

    additional file

---
+++ src/features/traveler/__tests__/Rest.test.ts
@@ -0,0 +1,42 @@
+import travelerReducer, {
+    repair,
+    rest,
+    steal,
+    reset,
+} from "../travelerSlice";
+import '@testing-library/jest-dom';
+import { initialState, zeroReputationState, negativeReputationState, restedState, disrepairState, suppliesState } from "./test-data/testStatesData";
+
+
+
+describe('rest reducer', () => {
+    test('should return intialState without any changes if payload is a negative number', () => {
+        expect(travelerReducer(initialState, rest(-11))).toBe(initialState);
+    });
+    test('should return intialState without any changes if payload is a zero', () => {
+        expect(travelerReducer(initialState, rest(0))).toBe(initialState);
+    });
+    test('should return zeroReputationState without any changes if reputation is a zero', () => {
+        expect(travelerReducer(zeroReputationState, rest(2))).toBe(zeroReputationState);
+    });
+    test('should return negativeReputationState without any changes if reputation is a negative number', () => {
+        expect(travelerReducer(negativeReputationState, rest(3))).toBe(negativeReputationState);
+    });
+    test('should return initialState without any changes if payload is greater than supplies', () => {
+        expect(travelerReducer(initialState, rest(6))).toBe(initialState);
+    });
+    test('should return restedState without any changes if fatigue is less than or equal to 100', () => {
+        expect(travelerReducer(restedState, rest(1))).toBe(restedState);
+    });
+    test('should update supplies, days, fatigue and disrepair if reputation is not less than or equal to zero and supplies are not less than payload', () => {
+        const result = {
+            cash: 100,
+            days: 2,
+            supplies: 2,
+            fatigue: 2,
+            disrepair: 6,
+            reputation: 100,
+        }
+        expect(travelerReducer(suppliesState, rest(1))).toStrictEqual(result);
+    });
+});
